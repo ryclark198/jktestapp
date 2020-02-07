50f6d998efd3dc9ca72d92e60d1d747bbadb51a2
ryclark198 <43451102+ryclark198@users.noreply.github.com>
2020-02-07 10:55:51 -0500

    3rd commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var math = require('./math');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.get('/another', function(req, res, next) {
   res.render('another', { title: 'Because he was dead!' });
+});
+// /api/add?n1=xn2=
+router.get('/api/add', function(req,res, next) {
+  var n1 =parseInt(req.query.n1);
+  var n2 =parseInt(req.query.n2);
+  res.json(math.add(n1,n2));
 });
 
 module.exports = router;
---
+++ routes/math.js
@@ -0,0 +1,6 @@
+var math = {};
+math.add = function(n1,n2) {
+    return n1+n2;
+};
+
+module.exports =math;
